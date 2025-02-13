effe8433c0643505c7f1cfe4cc63f1707d73bdda
Nikola Limpet <2023465lim@aupp.edu.kh>
2025-02-13 15:00:53 +0700

    fix instruction cart page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const verifyJWT = require('../middlewares/verifyJWT');
+const Acc = require('../models/user');
 
+router.get('/', async (req, res) => {
+  const allAcc = await Acc.find({});
+  res.json(allAcc).status(200);
+});
 router.use(verifyJWT);
 
 router.get('/profile', userController.getProfile);
