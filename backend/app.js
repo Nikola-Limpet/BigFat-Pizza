23f86fb26b45fe51faa61d00d15632fdcc832cca
Nikola Limpet <2023465lim@aupp.edu.kh>
2025-02-18 21:46:46 +0700

    change origin

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ app.use(
     origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
